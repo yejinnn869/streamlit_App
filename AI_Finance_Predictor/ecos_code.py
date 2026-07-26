c839ed7f69190e4d6919a3737abe40a07f2f4629
yejinnn869 <gimm3129@gmail.com>
2026-07-27 01:38:11 +0900

    Update ecos_code.py

--- AI_Finance_Predictor/ecos_code.py
+++ AI_Finance_Predictor/ecos_code.py
@@ -3,57 +3,43 @@
 ECOS_INDICATORS = {
 
     "cd91": {
-        "name": "CD(91일)",
-        "stat_code": "721Y001",
-        "item_code1": "실제항목코드1",
-        "item_code2": "",
-        "item_code3": "",
-        "cycle": "M"
+    "STAT_CODE": "721Y001",
+    "STAT_NAME": "1.3.2.2. 시장금리(월,분기,년)",
+    "ITEM_CODE1": "2010000",
+    "ITEM_NAME1": "CD91",   # 실제 ECOS 코드로 교체
+    "CYCLE": "M"
     },
 
     "cpi": {
-        "name": "소비자물가지수",
-        "stat_code": "실제통계표코드",
-        "item_code1": "실제항목코드1",
-        "item_code2": "",
-        "item_code3": "",
-        "cycle": "M"
+    "STAT_CODE": "901Y009",
+    "STAT_NAME": "4.2.1. 소비자물가지수",
+    "ITEM_CODE1": "0",
+    "ITEM_NAME!": "소비자물가지수",   # 실제 ECOS 코드로 교체
+    "CYCLE": "M"
     },
 
     "exchange": {
-        "name": "원/달러 환율",
-        "stat_code": "실제통계표코드",
-        "item_code1": "실제항목코드1",
-        "item_code2": "",
-        "item_code3": "",
+        "STAT_CODE": "731Y004",
+        "STAT_NAME": "3.2.1.2. 주요국 통화의 대원화환율",
+        "ITEM_CODE1": "0000001",
+        "ITEM_NAME1": "원/미국달러(매매기준율)",
         "cycle": "M"
     },
 
     "m2": {
-        "name": "M2",
-        "stat_code": "실제통계표코드",
-        "item_code1": "실제항목코드1",
-        "item_code2": "",
-        "item_code3": "",
+        "STAT_CODE": "161Y013",
+        "STAT_NAME": "1.1.3.3.1. M2 기관별 구성내역(평잔)",
+        "ITEM_CODE1": "BBJA00",
+        "ITEM_NAME1": "M2(평잔)",
         "cycle": "M"
     },
 
     "household_credit": {
-        "name": "가계신용",
-        "stat_code": "실제통계표코드",
-        "item_code1": "실제항목코드1",
-        "item_code2": "",
-        "item_code3": "",
-        "cycle": "Q"
+      "STAT_CODE": "151Y002",
+        "STAT_NAME": "1.2.4.2.1. 예금취급기관 가계대출(업권별,월),
+        "ITEM_CODE1": "1110000",
+        "ITEM_NAME1": "예금취급기관",
+        "cycle": "M"
     },
-
-    "gdp": {
-        "name": "GDP",
-        "stat_code": "실제통계표코드",
-        "item_code1": "실제항목코드1",
-        "item_code2": "",
-        "item_code3": "",
-        "cycle": "Q"
-    }
 
 }
