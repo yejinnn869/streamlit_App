b2872319d2fe94066fd3ed7261553f2489781a75
yejinnn869 <gimm3129@gmail.com>
2026-07-27 00:58:55 +0900

    Create ecos_code.py

---
+++ AI_Finance_Predictor/ecos_code.py
@@ -0,0 +1,59 @@
+# ecos_codes.py
+
+ECOS_INDICATORS = {
+
+    "cd91": {
+        "name": "CD(91일)",
+        "stat_code": "실제통계표코드",
+        "item_code1": "실제항목코드1",
+        "item_code2": "",
+        "item_code3": "",
+        "cycle": "M"
+    },
+
+    "cpi": {
+        "name": "소비자물가지수",
+        "stat_code": "실제통계표코드",
+        "item_code1": "실제항목코드1",
+        "item_code2": "",
+        "item_code3": "",
+        "cycle": "M"
+    },
+
+    "exchange": {
+        "name": "원/달러 환율",
+        "stat_code": "실제통계표코드",
+        "item_code1": "실제항목코드1",
+        "item_code2": "",
+        "item_code3": "",
+        "cycle": "M"
+    },
+
+    "m2": {
+        "name": "M2",
+        "stat_code": "실제통계표코드",
+        "item_code1": "실제항목코드1",
+        "item_code2": "",
+        "item_code3": "",
+        "cycle": "M"
+    },
+
+    "household_credit": {
+        "name": "가계신용",
+        "stat_code": "실제통계표코드",
+        "item_code1": "실제항목코드1",
+        "item_code2": "",
+        "item_code3": "",
+        "cycle": "Q"
+    },
+
+    "gdp": {
+        "name": "GDP",
+        "stat_code": "실제통계표코드",
+        "item_code1": "실제항목코드1",
+        "item_code2": "",
+        "item_code3": "",
+        "cycle": "Q"
+    }
+
+}
